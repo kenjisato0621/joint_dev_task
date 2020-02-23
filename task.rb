9a30f746dee486c4b2e4dd2185d9c71be9a35ce4
kenjisato0621 <ks01280719@gmail.com>
2020-02-23 20:52:35 -0300

    Q9.10.11

--- task.rb
+++ task.rb
@@ -72,21 +72,32 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+    names.each_with_index do |name,i|
+      puts "会員NO.#{i+1} #{name}さん"
+    end
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+   foods.each do |food|
+    if food.include?("うに")
+       puts "好物です"
+    else
+       puts "まぁまぁ好きです"
+    end 
+  end
 end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-
+　sports1 = sports.flatten.uniq
+  sports1.each.with_index(1) do |sport,i|
+   puts "#{i}#{sport}"
+    end
 end
 
 def q12
